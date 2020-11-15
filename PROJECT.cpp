dcac820f8c1ebb762eb7bd42ca6f90c783615841
sunkue <wmwwmmm@naver.com>
2020-11-15 17:13:50 +0900

    뱅기 움직임, 스텔스효과, 카메라모드 3개.

--- PROJECT.cpp
+++ PROJECT.cpp
@@ -14,9 +14,13 @@
 #include<gl/glm/glm/gtc/matrix_transform.hpp>
 #include"stb_image.h"
 /*
-	tmp : space arrow
-	23 25 : ` x y s
-	24 : tab q
+	light : space 
+	camera : 1 2 3
+	speed w s
+	rotate a d 8(up) 4(left) 5(down) 6(right) 
+	stealth 0
+	snow Q
+	building tap
 */
 constexpr glm::mat4 df(1.0f);
 
@@ -129,6 +133,7 @@ GLvoid MouseWheel(int button, int dir, int x, int y);
 GLvoid Timer(int value);
 GLvoid MakeShape();
 GLvoid DefaultObj();
+GLvoid print_message();
 /*struct funcs*/
 void Obj::Set_Alpha(const GLfloat alpha) {
 	for (std::vector<Vertex>::iterator i{ this->objData.vertices.begin() }, e{ this->objData.vertices.end() }; i != e; i++) {
@@ -147,42 +152,35 @@ void Obj::Set_Color(const glm::vec4& color) {
 /*user data*/
 struct PLANE {
 	Obj obj;
-	Obj coor[3];
 	glm::vec3 pos{ 0.0,0.0,0.0 };
-
 	glm::vec3 head{ 0.0,0.0,-1.0 };
 	glm::vec3 tail{ 0.0,0.0,1.0 };
 	glm::vec3 up{ 0.0,1.0,0.0 };
+
+	size_t eye_mode = 0;
 
 	GLfloat speed{ 0.0f };
 	GLfloat maxspeed{ 20.0f };
 	void init() {
 		// 0 Trans	1 Yaw	2 Pitch	3 Roll	4 Size
-		this->obj.M.resize(5, df);
+		this->obj.M.resize(3, df);
 		LoadObj("plane.txt", this->obj, "8/8");
-		this->obj.M.at(4) = glm::scale(df, glm::vec3(0.2));
-		setPos();
-		this->obj.Set_Color({ 0.0,0.0,1.0,1.0 });
-
-		/**/
-		for (int i = 0; i < 3; i++) {
-			coor[i].M.resize(1);
-			LoadObj("sphere.obj", coor[i], "8/8/8");
-		}
-		coor[0].Set_Color({ 1.0,0.0,0.0,1.0 });
-		coor[1].Set_Color({ 1.0,0.0,1.0,1.0 });
-		coor[2].Set_Color({ 0.0,1.0,0.0,1.0 });
+		this->obj.M.at(2) = glm::scale(df, glm::vec3(0.02));
+		this->setPos();
+		this->default_color();
 	}
-	glm::vec3 head_() { return this->obj.M.at(0) * glm::vec4(-this->nDir(), 1.0f); }
-	glm::vec3 tail_() { return this->obj.M.at(0) * glm::vec4(this->nDir(), 1.0f); }
-	glm::vec3 up_() { return this->obj.M.at(0) * glm::vec4(this->Up(), 1.0f); }
 
-
-	
+	void default_color() {
+		this->obj.Set_Color({ 0.5,0.0,1.0,1.0 });
+	}
+	void Stealth(bool on) {
+		if (on)obj.Set_Color({ 0.5,0.0,1.0,0.5 });
+		else this->default_color();
+	}
 	void update_coor(glm::mat4& m) {
-		head = glm::vec3(m * glm::vec4(head,1.0f));
-		tail = glm::vec3(m * glm::vec4(tail,1.0f));
-		up = glm::vec3(m * glm::vec4(up,1.0f));
+		head = glm::normalize(m * glm::vec4(head,1.0f));
+		tail = glm::normalize(m * glm::vec4(tail,1.0f));
+		up = glm::normalize(m * glm::vec4(up,1.0f));
 	}
 
 	glm::vec3 nDir() { return glm::normalize(this->tail - this->head); }
@@ -198,27 +196,27 @@ struct PLANE {
 	glm::mat4 roll_(GLfloat degree) {
 		return glm::rotate(df, glm::radians(degree), nDir());
 	}
+
 	void Yaw(GLfloat degree) {
 		glm::mat4 m = yaw_(degree);
-		obj.M.at(1) = obj.M.at(1) * m;
+		obj.M.at(1) = m*obj.M.at(1);
 		update_coor(m);
-		printf("y\n");
 	}
 	void Pitch(GLfloat degree) {
 		glm::mat4 m = pitch_(degree);
-		obj.M.at(2) = obj.M.at(2) * m;
+		obj.M.at(1) =m*obj.M.at(1);
 		update_coor(m);
-		printf("p\n");
 	}
 	void Roll(GLfloat degree) {
 		glm::mat4 m = roll_(degree);
-		obj.M.at(3) = obj.M.at(3) * m;
+		obj.M.at(1) = m*obj.M.at(1);
 		update_coor(m);
-		printf("r\n");
 	}
-	
+
 	void go() {
-		this->pos = glm::translate(df, nDir() * -speed) * glm::vec4{ this->pos,1.0f };
+		glm::mat4 m = glm::translate(df, nDir() * -speed);
+		this->pos = m * glm::vec4{ this->pos,1.0f };
+		//update_coor(m);
 	}
 
 	void set_speed(GLfloat d) {
@@ -232,27 +230,28 @@ struct PLANE {
 	}
 
 	void view() {
-		glm::mat4 m = pitch_(60) * glm::translate(df, nDir() * 100.0f);
-
-		//camera.AT = this->pos;
-		//camera.EYE = m * glm::vec4{ this->pos ,1.0f };
+		glm::mat4 m;
+		switch (eye_mode)
+		{
+		case 0: {m = glm::translate(df, nDir() * 100.0f); break; }
+		case 1: {m= glm::translate(df, nDir()*400.0f); break;}
+		case 2: {m = glm::translate(df, Right() * 5.0f)* glm::translate(df, nDir() * 10.0f); break; }
+		}
+		camera.AT =glm::translate(df, nDir() * -15.0f)* glm::vec4(this->pos, 1.0f);
+		camera.EYE = m * glm::vec4{ this->pos ,1.0f };
 	}
 
-	void update() {
-		coor[0].M[0] = glm::translate(df, head*50.0f);
-		coor[1].M[0] = glm::translate(df, tail*50.0f);
-		coor[2].M[0] = glm::translate(df, up*50.0f);
+	void update() {	
+		this->view();
 		this->go();
 		this->setPos();
-		this->view();
-		//update_coor();
 	}
 }plane;
 
-
-Obj coordinate, world, ground, building[8];
+constexpr int buildingnum{ 50 };
+constexpr GLfloat groundsize{ 1000.0f };
+Obj coordinate, world, ground, building[buildingnum];
 std::vector<Obj> snow;
-bool c_p{ true }, tab{ false };
 
 /*-----MAIN--*/
 int main(int argc, char** argv) {
@@ -326,6 +325,19 @@ int main(int argc, char** argv) {
 	glutTimerFunc(50, Timer, 0);
 
 	glutMainLoop();
+
+	print_message();
+}
+GLvoid print_message() {
+	std::cout << "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n";
+	std::cout <<
+		"\
+		light[space]\n\
+		plane[w a s d 0 up(8) down(5) left(4) right(6)]\n\
+		camera[1 2 3]\n\
+		snow[q]\n\
+		";
+	std::cout << "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n";
 }
 GLvoid DefaultObj() {
 	/*world*/
@@ -372,18 +384,19 @@ GLvoid DefaultObj() {
 GLvoid MakeShape() {
 	plane.init();
 	{
-		for (int i = 0; i < 8; i++) {
+		for (int i = 0; i < buildingnum; i++) {
 			LoadObj("cube.txt", building[i], "8/8/8");
 			building[i].Set_Color({ 1.0f,1.0f,GLfloat(rand() % 10) / 10.0f,0.5f });
 			building[i].M.resize(2, df);
-			building[i].M.at(1) = glm::scale(df, { 6.0f,30.0f,6.0f });
+			building[i].M.at(1) = glm::scale(df, { 6.0f,50.0f,6.0f });
+			building[i].M.at(0) = glm::translate(df, { GLfloat(rand() % int(groundsize)*2) - groundsize,-40.0f,GLfloat(rand() % int(groundsize) * 2) - groundsize });
 		}
 	}
 	{
 		LoadObj("cube.txt", ground, "8/8/8");
 		LoadTexture(ground, "grass.jpg", 512, 512, 3);
 		ground.M.push_back(glm::translate(df, { 0.0,-60.0,0.0 }));
-		ground.M.push_back(glm::scale(df, { 100.0,10.0,100.0 }));
+		ground.M.push_back(glm::scale(df, { groundsize,10.0,groundsize }));
 	}
 	/**/
 };
@@ -728,28 +741,19 @@ GLvoid drawScene() {
 		drawObj(world);
 		glFrontFace(GL_CCW);
 		drawObj(plane.obj);
-		for (int i = 0; i < 3; i++) {
-			drawObj(plane.coor[i]);
-		}
+
 		/*불투명*/
 		drawObj(light.obj);
-		if (tab) {
-			drawObj(ground);
-			for (std::vector<Obj>::iterator i{ snow.begin() }, e{ snow.end() }; i != e; i++) {
-				drawObj(*i);
-			}
-		}
-		else {
-			drawObj(coordinate);
+		drawObj(ground);
+		for (std::vector<Obj>::iterator i{ snow.begin() }, e{ snow.end() }; i != e; i++) {
+			drawObj(*i);
 		}
 	}
 	/*alpha*/
 	{
 		/*투명 ALpha_objs 로 push 하면 정렬한 후 드로우 함*/
-		if (tab) {
-			for (int i = 0; i < 8; i++) {
-				Alpha_objs.push_back(&building[i]);
-			}
+		for (int i = 0; i < buildingnum; i++) {
+			Alpha_objs.push_back(&building[i]);
 		}
 	}
 	/*그리기 끝*/
@@ -759,70 +763,57 @@ GLvoid drawScene() {
 }
 
 /*이벤트 함수*/
-GLvoid Reshape(int w, int h) {
-	screen.width = w, screen.height = h;
-	glViewport(0, 0, screen.width, screen.height);
-}
-bool up, down, right, left, bl_x, bl_y, bl_snow;
-bool P_go, P_stop, P_YL, P_YR, P_RL, P_RR, P_PU, P_PD;
+bool P_go, P_stop, P_YL, P_YR, P_RL, P_RR, P_PU, P_PD, bl_snow,stealth;
 GLvoid Timer(int value) {
 	constexpr GLfloat degree{ 5.0f };
 	switch (value)
 	{
-	case 0: {
-
-		if (up) {
-			glm::mat4 R = glm::rotate(df, glm::radians(-degree), camera.Right());
-			camera.EYE = glm::vec3(R * glm::vec4(camera.EYE, 1.0f));
-			camera.UP = glm::vec3(R * glm::vec4(camera.UP, 1.0f));
-		}
-		if (down) {
-			glm::mat4 R = glm::rotate(df, glm::radians(degree), camera.Right());
-			camera.EYE = glm::vec3(R * glm::vec4(camera.EYE, 1.0f));
-			camera.UP = glm::vec3(R * glm::vec4(camera.UP, 1.0f));
-		}
-		if (right) {
-			glm::mat4 R = glm::rotate(df, glm::radians(degree), camera.Up());
-			camera.EYE = glm::vec3(R * glm::vec4(camera.EYE, 1.0f));
-			camera.UP = glm::vec3(R * glm::vec4(camera.UP, 1.0f));
-		}
-		if (left) {
-			glm::mat4 R = glm::rotate(df, glm::radians(-degree), camera.Up());
-			camera.EYE = glm::vec3(R * glm::vec4(camera.EYE, 1.0f));
-			camera.UP = glm::vec3(R * glm::vec4(camera.UP, 1.0f));
-		}
-		
-
+	case 0: {		
 		plane.update();
+		
 		// P_go, P_stop, P_YL, P_YR, P_RL, P_RR, P_PU, P_PD
+
+		if (stealth) {
+			constexpr int tm{ 50 };
+			constexpr int tm2{ 25 };
+			static int time{ tm };
+			if(tm2<time)plane.Stealth(true);
+			else if(time%2) {
+				plane.Stealth(true);
+			}
+			else {
+				plane.Stealth(false);
+			}
+			time--;
+			if (time < 0)stealth = false, time = tm;
+		}
+
 		if (P_go) {
-			plane.set_speed(0.002f);
+			plane.set_speed(0.005f);
 		}
 		if (P_stop) {
-			plane.set_speed(-0.005f);
+			plane.set_speed(-0.01f);
 		}
 		if (P_YL) {
-			plane.Yaw(degree);
+			plane.Yaw(degree/2);
 		}
 		if (P_YR) {
-			plane.Yaw(-degree);
+			plane.Yaw(-degree/2);
 		}
 		if (P_RL) {
-			plane.Roll(-degree);
-		}
-		if (P_RR) {
 			plane.Roll(degree);
 		}
+		if (P_RR) {
+			plane.Roll(-degree);
+		}
 		if (P_PU) {
-			plane.Pitch(degree);
+			plane.Pitch(degree/5);
 		}
 		if (P_PD) {
-			plane.Pitch(-degree);
+			plane.Pitch(-degree/5);
 		}
-		plane.view();
 
-
-		if (tab) {
+		
 			if (250 <= snow.size()) { snow.begin()->objData.DelObjData(); snow.erase(snow.begin()); }
 			for (std::vector<Obj>::iterator i{ snow.begin() }, e{ snow.end() }; i != e; i++) {
 				if ((i->world_M()* glm::vec4{ i->objData.vertices.at(0).pos, 1.0f }).y <= -45.0f) {
@@ -830,28 +821,7 @@ GLvoid Timer(int value) {
 				}
 				else i->M.at(0) = glm::translate(i->M.at(0), { 0.0,-1.0,0.0 });
 			}
-		}
-
-		if (up) {
-			glm::mat4 R = glm::rotate(df, glm::radians(-degree), camera.Right());
-			camera.EYE = glm::vec3(R * glm::vec4(camera.EYE, 1.0f));
-			camera.UP = glm::vec3(R * glm::vec4(camera.UP, 1.0f));
-		}
-		if (down) {
-			glm::mat4 R = glm::rotate(df, glm::radians(degree), camera.Right());
-			camera.EYE = glm::vec3(R * glm::vec4(camera.EYE, 1.0f));
-			camera.UP = glm::vec3(R * glm::vec4(camera.UP, 1.0f));
-		}
-		if (right) {
-			glm::mat4 R = glm::rotate(df, glm::radians(degree), camera.Up());
-			camera.EYE = glm::vec3(R * glm::vec4(camera.EYE, 1.0f));
-			camera.UP = glm::vec3(R * glm::vec4(camera.UP, 1.0f));
-		}
-		if (left) {
-			glm::mat4 R = glm::rotate(df, glm::radians(-degree), camera.Up());
-			camera.EYE = glm::vec3(R * glm::vec4(camera.EYE, 1.0f));
-			camera.UP = glm::vec3(R * glm::vec4(camera.UP, 1.0f));
-		}
+		
 		glutTimerFunc(50, Timer, value);
 		break;
 	}
@@ -914,11 +884,9 @@ GLvoid Motion(int x, int y) {
 GLvoid MouseWheel(int button, int dir, int x, int y) {
 	if (dir > 0)
 	{
-		camera.EYE -= glm::normalize(camera.EYE - camera.AT);
 	}
 	else
 	{
-		camera.EYE += glm::normalize(camera.EYE - camera.AT);
 	}
 	glutPostRedisplay();
 }
@@ -974,20 +942,28 @@ GLvoid Keyboard(unsigned char key, int x, int y) {
 	GLfloat GLx = { ((float)x / screen.width) * 2 - 1 }, GLy{ (-((float)y / screen.height) * 2) + 1 };
 	switch (key)
 	{
-	case 'z': {
-		up = true;
+	case '0': {
+		if (!stealth) {
+			stealth = true;
+		}
 		break;
 	}
-	case 'x': {
-		down = true;
+	case '\t': {
+		for (int i = 0; i < buildingnum; i++) {
+			building[i].M.at(0) = glm::translate(df, { GLfloat(rand() % int(groundsize)*2) - groundsize,-40.0f,GLfloat(rand() % int(groundsize) * 2) - groundsize });
+		}
 		break;
 	}
-	case 'c': {
-		left = true;
+	case '1': {
+		plane.eye_mode = 0;
 		break;
 	}
-	case 'v': {
-		right = true;
+	case '2': {
+		plane.eye_mode = 1;
+		break;
+	}
+	case '3': {
+		plane.eye_mode = 2;
 		break;
 	}
 	case 'W':
@@ -1025,12 +1001,7 @@ GLvoid Keyboard(unsigned char key, int x, int y) {
 	case '8': {
 		P_PU = true;
 		break;
-	}
-	case '~':
-	case '`': {
-		c_p = c_p ? false : true;
-		break;
-	}
+	}	
 	case ' ': {
 		if (light.ambient < 1.0f)light.ambient += 0.4f;
 		else light.ambient = 0.0f;
@@ -1039,19 +1010,8 @@ GLvoid Keyboard(unsigned char key, int x, int y) {
 	}
 	case 'Q':
 	case 'q': {
-		if (!tab)break;
 		bl_snow = bl_snow ? false : true;
 		glutTimerFunc(50, Timer, 241);
-		break;
-	}
-	case '\t': {
-		for (int i = 0; i < 8; i++) {
-			building[i].M.at(0)=glm::translate(df, { GLfloat(rand() % 200) - 100.0f,-40.0f,GLfloat(rand() % 200) - 100.0f });
-		}
-		tab = tab ? false : true;
-		static GLfloat A{ 10.0f };
-		camera.AT.y += A;
-		A *= -1;
 		break;
 	}
 	default: { break; }
@@ -1062,22 +1022,6 @@ GLvoid keyboard_up(unsigned char key, int x, int y) {
 	GLfloat GLx = { ((float)x / screen.width) * 2 - 1 }, GLy{ (-((float)y / screen.height) * 2) + 1 };
 	switch (key)
 	{
-	case 'z': {
-		up = false;
-		break;
-	}
-	case 'x': {
-		down = false;
-		break;
-	}
-	case 'c': {
-		left = false;
-		break;
-	}
-	case 'v': {
-		right = false;
-		break;
-	}
 	case 'W':
 	case 'w': {
 		P_go = false;
@@ -1118,4 +1062,8 @@ GLvoid keyboard_up(unsigned char key, int x, int y) {
 		break;
 	}
 	glutPostRedisplay();
+}
+GLvoid Reshape(int w, int h) {
+	screen.width = w, screen.height = h;
+	glViewport(0, 0, screen.width, screen.height);
 }
